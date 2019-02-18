cf77e80bbbb977a963a28959cdc53a09fa45c809
sirharis214 <hariskido214@gmail.com>
2019-02-18 11:58:59 -0500

    Updating mysql connection

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 
-$mydb = new mysqli('127.0.0.1','root','12345','testdb');
+$mydb = new mysqli('127.0.0.1','test','12345678','test');
 
 if ($mydb->errno != 0)
 {
@@ -11,7 +11,7 @@ if ($mydb->errno != 0)
 
 echo "successfully connected to database".PHP_EOL;
 
-$query = "select * from students;";
+$query = "select * from testTable;";
 
 $response = $mydb->query($query);
 if ($mydb->errno != 0)
